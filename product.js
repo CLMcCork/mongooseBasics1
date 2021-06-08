4b76be82917cdf6a25854291cb05ab983c061e03
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-08 14:41:35 -0500

    updated product.js

--- product.js
+++ product.js
@@ -9,3 +9,29 @@ mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, us
     console.log("Oh no! An error has occurred!")
     console.log(err)
 })
+
+//can make the schema objects like this (good for validation) 
+//or can make schema like the one in index.js example 
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+const bike = new Product({ name: 'Moutain Bike', price: 999, color: 'red' })
+bike.save()
+.then(data => {
+    console.log("It worked!!!")
+    console.log(data)
+})
+.catch(err => {
+    console.log("Oh no! Error!")
+    console.log(err)
+})
