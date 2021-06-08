9e8f738933adf9c7f78f24d015f89a5c9283ba03
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-07 22:22:47 -0500

    updated code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+//require mongoose 
 const mongoose = require('mongoose'); 
+//connect to mongoose, use .then and .catch for error handliong 
 mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
     console.log("Connection open!!")
@@ -7,3 +9,28 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, u
     console.log("Oh no! An error has occurred!")
     console.log(err)
 })
+
+//define a schema 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String 
+});
+
+//take schema and tell mongoose to make a model using that schema 
+const Movie = mongoose.model('Movie', movieSchema);
+// const beautyBeast = new Movie({title: 'Beauty & the Beast', year: 1990, score: 10, rating: 'G'});
+
+
+// Movie.insertMany([
+//     { title: 'Aladdin', year: 1992, score: 8.3, rating: 'G'},
+//     { title: 'The Lion King', year: 1994, score: 8.3, rating: 'G'},
+//     { title: 'The Little Mermaid', year: 1883, score: 8.3, rating: 'G'},
+//     { title: 'A Goofy Movie', year: 2001, score: 8.3, rating: 'PG'},
+//     { title: 'Toy Story', year: 2004, score: 8.3, rating: 'PG'},
+// ])
+// .then(data => {
+//     console.log("IT WORKED!");
+//     console.log(data);
+// })
