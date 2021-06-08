cc836c0dcb6765c743435476c706bd4299ec4fb1
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-07 20:05:59 -0500

    connected js file using mongoose to a mongodb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log("Connection open!!")
+})
+.catch(err => {
+    console.log("Oh no! An error has occurred!")
+    console.log(err)
+})
