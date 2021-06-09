4015ff033d887a8dcf49fbcf797f2e10191334d1
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-09 16:28:58 -0500

    added some mongoose middleware

--- person.js
+++ person.js
@@ -22,5 +22,19 @@ personSchema.virtual('fullName').get(function () {
 })
 
 
+//mongoose middleware functions 
+//this runs pre saved 
+personSchema.pre('save', async function() {
+    this.first = 'Yoooooooo';
+    this.last = 'Mama';
+    console.log("About to save!!!!")
+})
+
+//mongoose middleware 
+//this runs post saved 
+personSchema.post('save', async function() {
+    console.log("JUST SAVED!!!!")
+})
+
 //person model 
 const Person = mongoose.model('Person', personSchema);
